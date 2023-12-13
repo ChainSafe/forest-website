cd88eb82204b853311efde2ed0901e33b1a61fae
sweetpea22 <cindychau24@me.com>
2023-12-13 00:14:58 -0500

    Remove large brand assets folder

--- src/sections/Hero3/index.tsx
+++ src/sections/Hero3/index.tsx
@@ -14,7 +14,7 @@ export default function Hero3() {
           <h1 className="md:max-w-[680px]  text-6xl text-neutral-200">
             Premier Rust implementation for the Filecoin Protocol
           </h1>
-          <h3 className='md:max-w-[380px] mt-3 text-3xl text-neutral-400 tracking-tight'>Run Forest to access Filecoin, the decentralized storage network.</h3>
+          <h3 className='md:max-w-[500px] mt-3 text-3xl text-neutral-400 tracking-tight'>Run Forest to access Filecoin, the decentralized storage network.</h3>
         </div>
         <div className="py-4 flex items-center">
           <a className="mr-2 transition-all rounded-full border-[1px] border-brand-400 text-brand-400 px-8 py-1 text-lg font-medium shadow-sm hover:brightness-[80%]"  href="#download">Install</a>
